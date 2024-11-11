ebf9a29725d08720ade16c7e2d4272cabed47ac7
Cookie Monster <cookiemonster@Scottyguytumblr.local>
2024-11-11 09:57:17 +0200

    profiles fixes

--- models/User.js
+++ models/User.js
@@ -2,124 +2,121 @@ const mongoose = require("mongoose");
 
 mongoose.set("strictPopulate", false);
 
-const userSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  age: {
-    type: String,
-    required: false,
-  },
-  gender: {
-    type: String,
-    enum: ["male", "female", "other"],
-  },
-
-  verified: {
-    type: Boolean,
-    default: false,
-  },
-  VerificatiionToken: String,
-  crushes: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-  ],
-
-  recievedLikes: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-  ],
-
-  profileDislikes: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
+    password: {
+      type: String,
+      required: true,
     },
-  ],
-  blockedBy: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-    },
-  ],
-
-  Matches: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-    },
-  ],
-  profileImages: {
-    type: [String],
-    default: [],
-  },
-
-  description: {
-    type: String,
-  },
-  location: {
-    type: String,
-  },
-  pushToken: {
-    type: String,
-  },
-
-  interests: {
-    type: [String],
-    default: [],
-  },
-
-  lookingFor: {
-    type: [String],
-    enum: ["Friendship", "Goodtime", "Long term relationship"],
-    default: [],
-  },
-  VerificationToken: {
-    type: String,
-  },
-  otp: {
-    code: {
+    age: {
       type: String,
       required: false,
     },
-    expires: {
-      type: Date,
-      required: false,
-    },
-  },
-  location: {
-    type: {
+    gender: {
       type: String,
-      enum: ["Point"],
-      required: false,
+      enum: ["male", "female", "other"],
     },
-    coordinates: {
-      type: [Number],
-      required: false,
-      index: "2dsphere",
+    verified: {
+      type: Boolean,
+      default: false,
     },
+    VerificatiionToken: String,
+    crushes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    recievedLikes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    profileDislikes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    blockedBy: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    Matches: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    profileImages: {
+      type: [String],
+      default: [],
+    },
+    description: {
+      type: String,
+    },
+    location: {
+      type: String,
+    },
+    pushToken: {
+      type: String,
+    },
+    interests: {
+      type: [String],
+      default: [],
+    },
+    lookingFor: {
+      type: [String],
+      enum: ["Friendship", "Goodtime", "Long term relationship"],
+      default: [],
+    },
+    VerificationToken: {
+      type: String,
+    },
+    otp: {
+      code: {
+        type: String,
+        required: false,
+      },
+      expires: {
+        type: Date,
+        required: false,
+      },
+    },
+    location: {
+      type: {
+        type: String,
+        enum: ["Point"],
+        required: false,
+      },
+      coordinates: {
+        type: [Number],
+        required: false,
+        index: "2dsphere",
+      },
+    },
+    conversations: [
+      {
+        receiverId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+        unreadMessagesCount: { type: Number, default: 0 },
+      },
+    ],
   },
-  conversations: [
-    {
-      receiverId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-      unreadMessagesCount: { type: Number, default: 0 },
-    },
-  ],
-});
+  { timestamps: true }
+); // Add timestamps option here
+
 userSchema.index({ location: "2dsphere" });
 const User = mongoose.model("User", userSchema);
 
